f65447c385c021f2eb1c870a40d366f663d0adf8
e1-one <vanya.yarovoy@gmail.com>
2020-04-20 14:19:57 +0300

    vtgbt case with 0 is handled

---
+++ src/helper/test_values_to_graph_block_translator.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+
+from helper.values_to_graph_block_translator import ValuesToGraphBlockTranslator
+
+
+class TestVTG(TestCase):
+
+    def test_happy_path(self):
+        vtg = ValuesToGraphBlockTranslator([1, 2, 3, 4, 5, 6])
+        self.assertEqual(vtg.get(1), "▏")
+        self.assertEqual(vtg.get(2), "▎")
+        self.assertEqual(vtg.get(3), "▍")
+        self.assertEqual(vtg.get(4), "▋")
+        self.assertEqual(vtg.get(5), "▊")
+        self.assertEqual(vtg.get(6), "▉")
+    pass
+
+    def test_mixed_order(self):
+        vtg = ValuesToGraphBlockTranslator([5, 1, 2.5])
+        self.assertEqual(vtg.get(1), "▏")
+        self.assertEqual(vtg.get(5), "▉")
+        self.assertEqual(vtg.get(4.5), "▊")
+    pass
+
+    def test_list_with_zero(self):
+        vtg = ValuesToGraphBlockTranslator([0, 1, 10, 0])
+        self.assertEqual(vtg.get(1), "▏")
+        self.assertEqual(vtg.get(10), "▉")
+        self.assertEqual(vtg.get(0), ' ')
+    pass
+
+    def test_list_with_zero(self):
+        vtg = ValuesToGraphBlockTranslator([0, 1, 10, 0])
+        self.assertEqual(vtg.get(1), "▏")
+        self.assertEqual(vtg.get(10), "▉")
+        self.assertEqual(vtg.get(0), ' ')
+    pass
--- src/helper/values_to_graph_block_translator.py
+++ src/helper/values_to_graph_block_translator.py
@@ -1,18 +1,27 @@
-
 def map_value(x: float, in_min: float, in_max: float, out_min: float, out_max: float):
     if in_max == in_min:
         return 0
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
+def unique(list1):
+    list_set = set(list1)
+    return list(list_set)
+
+
 class ValuesToGraphBlockTranslator:
     squares = ['▏', '▎', '▍', '▋', '▊', '▉']
+    no_data = ' '
 
     def __init__(self, values: list):
-        self.max = max(values)
-        self.min = min(values)
+        sorted_values = sorted(unique(values))
+
+        self.min = sorted_values[0] if sorted_values[0] is not 0 else sorted_values[1]
+        self.max = sorted_values[len(sorted_values)-1]
 
     def get(self, value: float):
+        if value == 0:
+            return ValuesToGraphBlockTranslator.no_data
         index = int(map_value(value, self.min, self.max, 0, len(ValuesToGraphBlockTranslator.squares) - 1))
         return ValuesToGraphBlockTranslator.squares[index]
 

9231d75878b4b7abe4d11f008895207ccb9d0221
e1-one <vanya.yarovoy@gmail.com>
2020-06-19 10:06:00 +0300

    the token is removed from the run-proc script.

--- runbot-proc.sh
+++ runbot-proc.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# crontab -e
-# add: 30 * * * * $PATH_TO_THIS_FOLDER/runbot-proc.sh
+# How to start the process with the bot
+# 1) chmod 755 runbot-proc.sh
+# 2) crontab -e
+# 3) add: 30 * * * * $PATH_TO_THIS_FOLDER/runbot-proc.sh
 # How to check CRON log:
 # grep CRON /var/log/syslog
+
+# script expects BOT_TOKEN as env variable.
+
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 ps -aux | grep -v grep | grep start_bot_service.py
 if [ $? -eq 0 ]; then
   echo "Process is already running."
 else
   echo "Process is not running. Let's start it!"
-  nohup python3.7 ./exchange-telegram-bot/src/start_bot_service.py 1026738267:AAHmqP-Qzo8H1bC6M93o6PSgqFg8qsCNQyo > bot-nohup.out &
+  nohup python "$THIS_DIR/src/start_bot_service.py" "$BOT_TOKEN" > bot-nohup.out &
 fi
